607d29aa2bea9929133f9780c0ec9abe134ec9ff
coolsuraj665 <53422906+coolsuraj665@users.noreply.github.com>
2020-01-20 08:08:29 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">SurajTech</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active"> 
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php ">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>Bootstrap Tutorial</h1>
+  <p>Bootstrap is the most popular HTML, CSS...</p>
+</div>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-6 col-md-6 col-12">
+      <img src="img/image3.jpg" class="img-fluid aboutimg">
+    </div>
+    <div class="col-lg-6 col-md-6 col-12">
+      <h2>I am Suraj Technical.</h2>
+      <p class="py-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <a href="about.php" class="btn btn-outline-success"> check More</a>
+    </div>
+
+  </div>
+  </div>
+  
+</section>
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center" >@surajtechnicalProduction</p>
+</footer>
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">SurajTech</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active"> 
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php ">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/image1.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        
+        
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/image2.jpg" class="d-block w-100 h-100" alt="">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/image3.jpg" class="d-block w-100 h-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About us</h2>
+	</div>
+	<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-6 col-md-6 col-12">
+			<img src="img/image3.jpg" class="img-fluid aboutimg">
+		</div>
+		<div class="col-lg-6 col-md-6 col-12">
+			<h2>I am Suraj Technical.</h2>
+			<p class="py-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			<a href="about.php" class="btn btn-outline-success"> check More</a>
+		</div>
+
+	</div>
+	</div>
+	
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Our Services</h2>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-log-4 col-md-4 col-12">
+				<div class="card" >
+                   <img class="card-img-top" src="img/image2.jpg" alt="Card image">
+                    <div class="card-body">
+                        <h4 class="card-title">Beatiful Nature</h4>
+                           <p class="card-text">Some example text.</p>
+    							<a href="#" class="btn btn-primary">See Profile</a>
+  					</div>
+ 				</div>
+				
+			</div>
+
+			<div class="col-log-4 col-md-4 col-12">
+				<div class="card" >
+                   <img class="card-img-top" src="img/image2.jpg" alt="Card image">
+                    <div class="card-body">
+                        <h4 class="card-title">Beatiful Nature</h4>
+                           <p class="card-text">Some example text.</p>
+    							<a href="#" class="btn btn-primary">See Profile</a>
+  					</div>
+ 				</div>
+				
+			</div>
+
+			<div class="col-log-4 col-md-4 col-12">
+				<div class="card" >
+                   <img class="card-img-top" src="img/image2.jpg" alt="Card image">
+                    <div class="card-body">
+                        <h4 class="card-title">Beatiful Nature</h4>
+                           <p class="card-text">Some example text.</p>
+    							<a href="#" class="btn btn-primary">See Profile</a>
+  					</div>
+ 				</div>
+				
+			</div>
+			
+		</div>
+		
+	</div>
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Our Gallary</h2>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-log-4 col-md-4 col-12">
+			<img src="img/image1.jpg" class="img-fluid pb-3">
+			</div>
+				
+			
+			
+		</div>
+		
+	</div>
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Our Gallary</h2>
+	</div>
+	<div class="w-50 m-auto">
+		<form action="userinfo.php" method="post">
+			<div class="form-group">
+				<label>username</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Email-Id</label>
+				<input type="text" name="email" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Mobile No</label>
+				<input type="text" name="mobile" autocomplete="off" class="form-control">
+			</div>
+			<div class="form-group">
+				<label>Comments</label>
+				<textarea class="form-control" name="Comments">
+					
+				</textarea>
+			</div>
+			<button type="submit" class="btn btn-success">Submit</button>
+		</form>
+	</div>
+</section>
+
+<footer>
+	<p class="p-3 bg-dark text-white text-center" >@surajtechnicalProduction</p>
+</footer>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
